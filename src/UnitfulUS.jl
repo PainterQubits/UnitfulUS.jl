b54b38d9b492ce06480ff03a052ce97de94a3923
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-01-05 10:34:32 +0100

    fix issue that occurs from change in serialization format in Julia 1.9

--- src/UnitfulUS.jl
+++ src/UnitfulUS.jl
@@ -47,7 +47,7 @@ include("usmacro.jl")
 
 # Some gymnastics required here because if we precompile, we cannot add to
 # Unitful.basefactors at compile time and expect the changes to persist to runtime.
-const localunits = Unitful.basefactors
+const localunits = copy(Unitful.basefactors)
 function __init__()
     merge!(Unitful.basefactors, localunits)
     Unitful.register(UnitfulUS)
